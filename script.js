32105c841b61f44b143a7cdeeb9d4f4fbd0d487f
unknown <yoannbouclierpro@gmail.com>
2021-11-04 14:49:41 +0100

    'version1'

---
+++ script.js
@@ -0,0 +1,15 @@
+
+let perso = { vie : 10, degats : 5, PA: 6};
+let perso2 = { vie : 10, degats : 10, PA: 6};
+
+
+function bouleDeFeu(pa, degats) {
+    console.log(perso.PA - pa)
+    console.log(perso2.vie - degats)
+}
+
+
+
+bouleDeFeu(2, 8)
+
+
